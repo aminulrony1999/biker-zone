cd731741520fc0f9fce26d6a8693a21a3054c8fe
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-20 16:15:49 +0530

    carousel added from daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,59 @@
 import React from "react";
 import "./App.css";
+import bike1 from "../src/assets/images/slider/bike1.png";
+import bike2 from "../src/assets/images/slider/bike2.png";
+import bike3 from "../src/assets/images/slider/bike3.png";
+import bike4 from "../src/assets/images/slider/bike4.png";
 const App = () => {
   return (
     <div>
       <header className="md:container md:mx-auto">
-      <nav>
-        <div className="navbar bg-base-100">
-          <div className="flex-1">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+        <nav>
+          <div className="navbar bg-base-100">
+            <div className="flex-1">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm text-base dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <a>Home</a>
+                  </li>
+                  <li>
+                    <a>Shop</a>
+                  </li>
+                  <li>
+                    <a>News</a>
+                  </li>
+                  <li>
+                    <a>Contact</a>
+                  </li>
+                </ul>
               </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm text-base dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-              >
+              <a className="btn btn-ghost text-4xl">Biker Zone</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal text-base px-1">
                 <li>
                   <a>Home</a>
                 </li>
@@ -46,29 +68,58 @@ const App = () => {
                 </li>
               </ul>
             </div>
-            <a className="btn btn-ghost text-4xl">Biker Zone</a>
+            <div>
+              <a className="btn bg-bike-primary text-white">Login</a>
+            </div>
           </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal text-base px-1">
-              <li>
-                <a>Home</a>
-              </li>
-              <li>
-                <a>Shop</a>
-              </li>
-              <li>
-                <a>News</a>
-              </li>
-              <li>
-                <a>Contact</a>
-              </li>
-            </ul>
+        </nav>
+        {/* Silder or Carousel */}
+        <div className="carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img src={bike1} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div>
-            <a className="btn bg-bike-primary text-white">Login</a>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img src={bike2} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img src={bike3} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img src={bike4} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
         </div>
-      </nav>
       </header>
       <main className="md:container md:mx-auto"></main>
       <footer></footer>
