df899804ffd3550d73f302a9d5f39b1dacd4960f
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-21 20:26:29 +0530

    image imported and displayed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import latest1 from "../assets/images/others/latest1.png";
+import latest2 from "../assets/images/others/latest2.png";
+import latest3 from "../assets/images/others/latest3.png";
 const Main = () => {
   return (
     <div className="md:container md:mx-auto">
@@ -11,10 +14,33 @@ const Main = () => {
           {/* first card */}
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure>
-              <img
-                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                alt="Shoes"
-              />
+              <img src={latest3} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          {/* second card */}
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img src={latest2} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          {/* third card */}
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img src={latest3} alt="Shoes" />
             </figure>
             <div className="card-body">
               <h2 className="card-title">Shoes!</h2>
