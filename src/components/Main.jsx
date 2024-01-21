59101f97773731057a3d672bf04fe389bbbbeebc
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-21 23:20:02 +0530

    happy clients part completed with responsiveness

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import latest1 from "../assets/images/others/latest1.png";
 import latest2 from "../assets/images/others/latest2.png";
 import latest3 from "../assets/images/others/latest3.png";
+import user1 from "../assets/images/others/user-1.png";
+import user2 from "../assets/images/others/user-2.png";
+import user3 from "../assets/images/others/user-3.png";
 const Main = () => {
   return (
     <div className="md:container md:mx-auto">
@@ -9,7 +12,9 @@ const Main = () => {
       <section></section>
       {/* latest bike */}
       <section className="my-32">
-        <h3 className="text-5xl text-center font-semibold mb-6">Latest Bikes</h3>
+        <h3 className="text-5xl text-center font-semibold mb-6">
+          Latest Bikes
+        </h3>
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {/* first card */}
           <div className="card bg-base-100 shadow-xl">
@@ -62,6 +67,83 @@ const Main = () => {
                 <button className="btn btn-primary bg-bike-primary text-white border-none">
                   Read More
                 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Happy Clients */}
+      <section className="my-32">
+        <h3 className="text-5xl text-center font-semibold mb-6">
+          Happy <span className="text-bike-primary">Clients say</span>
+        </h3>
+        <div className="grid grid-cols-1 gap-11 md:grid-cols-2 lg:grid-cols-3">
+          {/* first review */}
+          <div className="card bg-base-100 p-9 border border-gray-400">
+            <figure>
+              <img className="w-36" src={user1} alt="user1" />
+            </figure>
+            <div className="card-body">
+              <p>
+                Slate helps you see how many more days you need to work to reach
+                your financial goal for the month and year.
+              </p>
+              <div className="rating justify-center">
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+              </div>
+              <div className="text-center space-y-4 mt-4">
+                <h4 className="text-sm text-info font-semibold">Regina Miles</h4>
+                <p>Banker</p>
+              </div>
+            </div>
+          </div>
+          {/* second review */}
+          <div className="card bg-base-100 p-9 border border-gray-400">
+            <figure>
+              <img className="w-36" src={user2} alt="user2" />
+            </figure>
+            <div className="card-body">
+              <p>
+                Slate helps you see how many more days you need to work to reach
+                your financial goal for the month and year.
+              </p>
+              <div className="rating justify-center">
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+              </div>
+              <div className="text-center space-y-4 mt-4">
+                <h4 className="text-sm text-info font-semibold">Regina Miles</h4>
+                <p>Banker</p>
+              </div>
+            </div>
+          </div>
+          {/* third review */}
+          <div className="card bg-base-100 p-9 border border-gray-400">
+            <figure>
+              <img className="w-36" src={user3} alt="user1" />
+            </figure>
+            <div className="card-body">
+              <p>
+                Slate helps you see how many more days you need to work to reach
+                your financial goal for the month and year.
+              </p>
+              <div className="rating justify-center">
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+              </div>
+              <div className="text-center space-y-4 mt-4">
+                <h4 className="text-sm text-info font-semibold">Regina Miles</h4>
+                <p>Banker</p>
               </div>
             </div>
           </div>
