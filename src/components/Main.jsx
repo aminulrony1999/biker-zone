2dcfd33f19f23672e31bccc8131d85a716f42187
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-22 01:14:12 +0530

    hero and accordion added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import latest3 from "../assets/images/others/latest3.png";
 import user1 from "../assets/images/others/user-1.png";
 import user2 from "../assets/images/others/user-2.png";
 import user3 from "../assets/images/others/user-3.png";
+import faq from "../assets/images/others/faq.png";
 const Main = () => {
   return (
     <div className="md:container md:mx-auto">
@@ -89,14 +90,37 @@ const Main = () => {
                 your financial goal for the month and year.
               </p>
               <div className="rating justify-center">
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
               </div>
               <div className="text-center space-y-4 mt-4">
-                <h4 className="text-sm text-info font-semibold">Regina Miles</h4>
+                <h4 className="text-sm text-info font-semibold">
+                  Regina Miles
+                </h4>
                 <p>Banker</p>
               </div>
             </div>
@@ -112,14 +136,37 @@ const Main = () => {
                 your financial goal for the month and year.
               </p>
               <div className="rating justify-center">
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
               </div>
               <div className="text-center space-y-4 mt-4">
-                <h4 className="text-sm text-info font-semibold">Regina Miles</h4>
+                <h4 className="text-sm text-info font-semibold">
+                  Regina Miles
+                </h4>
                 <p>Banker</p>
               </div>
             </div>
@@ -135,15 +182,114 @@ const Main = () => {
                 your financial goal for the month and year.
               </p>
               <div className="rating justify-center">
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
               </div>
               <div className="text-center space-y-4 mt-4">
-                <h4 className="text-sm text-info font-semibold">Regina Miles</h4>
+                <h4 className="text-sm text-info font-semibold">
+                  Regina Miles
+                </h4>
                 <p>Banker</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Frequent Asked Questions FAQ */}
+      <section className="my-32">
+        <h3 className="text-5xl text-center font-semibold mb-6">
+          Frequently Asked <span className="text-bike-primary">Questions</span>
+        </h3>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row">
+            <img src={faq} className="max-w-sm rounded-lg shadow-2xl" />
+            <div>
+              <div className="join join-vertical w-full">
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" checked="checked" />
+                  <div className="collapse-title text-xl font-medium">
+                    What are the most important things I should know about
+                    riding a bike?
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      Safety First! Always obey the rules of the road. Obey all
+                      traffic signals, signs, and laws. Get in the mindset of
+                      “driving” your bike—not just “riding” your bike. This will
+                      help you be a more focused and legally compliant bike
+                      rider.
+                    </p>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">
+                    How can I tell if my helmet is old and I need a new one?
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      Safety First! Always obey the rules of the road. Obey all
+                      traffic signals, signs, and laws. Get in the mindset of
+                      “driving” your bike—not just “riding” your bike. This will
+                      help you be a more focused and legally compliant bike
+                      rider.
+                    </p>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">
+                    My bike has been in storage is it safe to ride?
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      Safety First! Always obey the rules of the road. Obey all
+                      traffic signals, signs, and laws. Get in the mindset of
+                      “driving” your bike—not just “riding” your bike. This will
+                      help you be a more focused and legally compliant bike
+                      rider.
+                    </p>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">
+                    What rules should I follow when riding my bike?
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      Safety First! Always obey the rules of the road. Obey all
+                      traffic signals, signs, and laws. Get in the mindset of
+                      “driving” your bike—not just “riding” your bike. This will
+                      help you be a more focused and legally compliant bike
+                      rider.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
